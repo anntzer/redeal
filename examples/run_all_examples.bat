29cec290d61593ea696a8e8522db294c8b0b3a2f
Antony Lee <anntzer.lee@gmail.com>
2017-12-26 15:38:01 -0800

    Fix OSX build.

--- examples/run_all_examples.bat
+++ examples/run_all_examples.bat
@@ -1,7 +1,6 @@
 @echo off
 pushd %cd%
 chdir %~dp0
-chdir ..
 for %%f in (examples\*.py) do (
 echo %%f
 python -m redeal %%f
--- examples/run_all_examples.sh
+++ examples/run_all_examples.sh
@@ -2,5 +2,5 @@
 set -e
 # May fail if someone has this in his $PATH, but readlink -f doesn't work on
 # Mac OS, so at least it's portable.
-cd "$(dirname "$(pwd)/$0")"/..
-for f in `ls examples/*.py`; do echo $f; ${PYTHON:-python} -m redeal $f; done
+cd "$(dirname "$(pwd)/$0")"
+for f in *.py; do echo $f; ${PYTHON:-python} -m redeal $f; done
