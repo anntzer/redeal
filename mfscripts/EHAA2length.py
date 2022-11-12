f6286b3c5518551fd976382c07bf0c060945b80c
mycroftw <30244806+mycroftw@users.noreply.github.com>
2022-11-12 15:45:14 -0600

    Minor logic fixes (and problem solve) in EHAA2length.py.
    Logic fixes:
    - if "is_clubs", only count other suit if equal or longer than club suit
      (rather than >=5).
    - only store one "longest suit" per hand (the highest suit by fiat)
    - keep track of matches, and report on both total deals generated and
      total matched.
    
    Cleanup:
    - function typing.
    - PEP 8 issues (mostly "move initial setup to __init__()")
    - use Counter rather than generic dict.
    - x += 1 rather than x=x+1
    - use Python 3 key function sorting, and "guaranteed order" sorting
      rather than complicated comparison function.
    - pprint rather than dumb formatting.

--- mfscripts/EHAA2length.py
+++ mfscripts/EHAA2length.py
@@ -1,13 +1,16 @@
-import collections
-from functools import cmp_to_key
+from collections import Counter
+import pprint
 
-from redeal import Evaluator, hcp, Shape, Simulation
+from redeal import Evaluator, hcp, Shape, Simulation, Deal, Hand
 from redeal.global_defs import Suit
 
-DEBUG = {'hand': True,
-         'hcp': True,
-         'is_clubs': False,
-         'suits': True,}
+
+DEBUG = {
+    'hand': False,
+    'hcp': True,
+    'is_clubs': False,
+    'suits': False,
+}
 
 IS_CLUBS = True
 RANGE_MIN = 6
@@ -19,16 +22,7 @@ twobidshape = Shape.from_cond(
 full_suit_eval = Evaluator(*range(14, 1, -1))
 
 
-def holding_cmp(x, y):
-    """stronger holding.  Length, then cards from sum."""
-    return (
-        len(x) - len(y)
-        if len(x) != len(y)
-        else full_suit_eval(x) - full_suit_eval(y)
-    )
-
-
-def ehaa_nt(hand):
+def ehaa_nt(hand: Hand) -> bool:
     is_nt_shape = (Shape('(4333)') + Shape('(4432)')
                    + Shape('(33)(25)') + Shape('(32)(35)'))
     return is_nt_shape(hand) and hcp(hand) in (10, 11, 12)
@@ -36,54 +30,58 @@ def ehaa_nt(hand):
 
 class EHAA2length(Simulation):
 
-    def initial(self):
-        self.maxlength = collections.defaultdict(int)
-        self.points = collections.defaultdict(int)
+    def __init__(self):
+        super().__init__()
         self.bidsuit = []
+        self.maxlength = Counter()
+        self.points = Counter()
 
-    def accept(self, deal):
-
+    def accept(self, deal: Deal) -> bool:
         return (
                 deal.south.shape in twobidshape
                 and hcp(deal.south) in range(RANGE_MIN, RANGE_MAX + 1)
                 and not ehaa_nt(deal.south)
         )
 
-    def do(self, deal):
-
+    def do(self, deal: Deal) -> None:
         if DEBUG['hand']:
             print(deal.south)
 
         if DEBUG['is_clubs'] and len(deal.south.clubs) >= 5:
-            other_suit = (len(deal.south.diamonds) >= 5
-                          or len(deal.south.hearts) >= 5
-                          or len(deal.south.spades) >= 5)
+            other_suit = (len(deal.south.diamonds) >= len(deal.south.clubs)
+                          or len(deal.south.hearts) >= len(deal.south.clubs)
+                          or len(deal.south.spades) >= len(deal.south.clubs))
             print(f'{deal.south}   {other_suit}')
 
         ml = max(deal.south.shape)
-        self.maxlength[ml] = self.maxlength[ml] + 1
-        self.points[deal.south.hcp] = self.points[deal.south.hcp] + 1
+        self.maxlength[ml] += 1
+        self.points[deal.south.hcp] += 1
         for suit in Suit:
             if len(deal.south[suit]) == ml:
                 self.bidsuit.append(deal.south[suit])
+                break  # only store 1 longest suit (highest by fiat) per hand
 
-    def final(self, n_tries):
+    def final(self, n_tries: int) -> None:
 
         print('Lengths:')
-        for k in sorted(self.maxlength):
-            print(f'{k}\t{self.maxlength[k]}')
+        pprint.pprint(dict(self.maxlength), width=1)
 
         if DEBUG['hcp']:
             print('\nHCP:')
-            for k in sorted(self.points):
-                print(f'{k}\t{self.points[k]}')
+            pprint.pprint(dict(self.points), width=1)
 
+        # sort on secondary key first (strength of suit), then primary (length)
+        sorted_bidsuit = sorted(self.bidsuit, key=full_suit_eval, reverse=True)
+        sorted_bidsuit = sorted(sorted_bidsuit,
+                                key=lambda x: len(x),
+                                reverse=True)
         if DEBUG['suits']:
-            print(self.bidsuit)
-            print([full_suit_eval(x) for x in self.bidsuit])
+            pprint.pprint([(str(x), len(x), full_suit_eval(x))
+                           for x in sorted_bidsuit],
+                          width=100,
+                          compact=True)
 
-        print('\nMedian hand: '
-              f'{sorted(self.bidsuit, key=cmp_to_key(holding_cmp))[len(self.bidsuit) // 2]}')
+        print(f'\nMedian hand: {sorted_bidsuit[len(self.bidsuit) // 2]}')
 
 
 simulation = EHAA2length()
